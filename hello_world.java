68cbf0008aa0e40a37b89641e6712240f79827d1
Curry5004 <yunjin5004@naver.com>
2022-01-02 14:33:35 +0900

    333

--- hello_world.java
+++ hello_world.java
@@ -2,7 +2,7 @@ public class hello_world {
 	public static void main(String[] args) {
 		System.out.println("hello world");
 		
-		
+		System.out.println("hello world33");
 		System.out.println(plus(5,3));
 		
 	}

07c35d27c45b7ac05ba73400d016fd85d325ce6a
Curry5004 <yunjin5004@naver.com>
2022-01-02 14:25:43 +0900

    add hello_world.java [print]

--- hello_world.java
+++ hello_world.java
@@ -1,7 +1,7 @@
 public class hello_world {
 	public static void main(String[] args) {
 		System.out.println("hello world");
-		
+		System.out.println("hello world222");
 		
 		System.out.println(plus(5,3));
 		
